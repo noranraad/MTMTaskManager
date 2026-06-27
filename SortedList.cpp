bf6e55e8ffd7bddd8aee4ada744115179951b93a
noranraad <noran.raad@campus.technion.ac.il>
2026-06-27 07:50:59 +0300

    Implemnting the ConstIterator && Cstr, , CoCopy Cstr,Dstr, insert, length

---
+++ SortedList.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Noran on 6/27/2026.
+//
+#include "SortedList.h"
--- SortedList.h
+++ SortedList.h
@@ -4,10 +4,94 @@
 #include <stdexcept>
 
 namespace mtm {
+    class Exception : public std::out_of_range {
+    public:
+        Exception() : std::out_of_range("SortedList iterator out of range"){}
+    };
 
     template <typename T>
     class SortedList {
+        struct Node {
+            T data;
+            Node* next = nullptr;
+            Node(const T& value, Node* nextNode) : data(value), next(nextNode){}
+        };
+
+        Node<T>* head = nullptr;
+
     public:
+        SortedList() = default;
+
+        SortedList& insert(const T& elem) {
+            Node* newNode = new Node<T>{elem, nullptr};
+
+            if (head == nullptr || elem > head->data) {
+                newNode->next = head;
+                head = newNode;
+                return *this;
+            }
+
+            Node* curr = head;
+
+            while (curr->next != nullptr &&
+                   !(elem > curr->next->data)) {
+                curr = curr->next;
+                   }
+
+            newNode->next = curr->next;
+            curr->next = newNode;
+
+            return *this;
+        }
+
+        SortedList(const SortedList& other):head(nullptr) {
+            const Node<T>* curr = other.head;
+            while (curr != nullptr) {
+                insert(curr->data);
+                curr = curr->next;
+            }
+        }
+
+        SortedList& operator=(const SortedList& other) {
+            if (this == &other) {
+                return *this;
+            }
+
+            SortedList temp(other);
+
+            Node* tempHead = head;
+            head = temp.head;
+            temp.head = tempHead;
+
+            return *this;
+        }
+
+        ~SortedList() {
+            while (head != nullptr) {
+                Node* nodeToDelete = head;
+                head = head->next;
+                delete nodeToDelete;
+            }
+        }
+
+        
+
+        class ConstIterator;
+        typedef const char* constIterator;
+        constIterator begin() const;
+        constIterator end() const;
+
+        int length() const {
+            const Node<T>* curr = head;
+            int cnt = 0;
+            while (curr != nullptr) {
+                cnt++;
+                curr = curr->next;
+            }
+            return cnt;
+        }
+
+
         /**
          *
          * the class should support the following public interface:
@@ -36,6 +120,29 @@ namespace mtm {
 
     template <class T>
     class SortedList<T>::ConstIterator {
+        SortedList<T>* sortedList;
+        int index;
+
+    public:
+
+        ConstIterator() : sortedList(nullptr), index(0) {}
+
+        ConstIterator& operator++() {
+            index--;
+            if (index > SortedList<T>::length || index < 0) throw Exception();
+            return *this;
+        }
+
+        bool operator!=(const ConstIterator& other) const{
+            if (index > SortedList<T>::length || index < 0) throw Exception();
+            return this->index != other.index;
+        }
+
+        T* operator*() const{
+            if (index > SortedList<T>::length || index < 0) throw Exception();
+            return sortedList->head->data[index];
+        }
+
     /**
      * the class should support the following public interface:
      * if needed, use =defualt / =delete
